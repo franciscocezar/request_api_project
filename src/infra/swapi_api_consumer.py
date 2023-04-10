80f478c7e2c8148d4e02657d13eed6d015b26d69
Francisco Cezar <franciscocezar.jr@outlook.com>
2023-04-10 09:48:05 -0300

    feat: Implementing error in swapi_api_consumer and testing with mocks

--- src/infra/swapi_api_consumer.py
+++ src/infra/swapi_api_consumer.py
@@ -1,14 +1,21 @@
-from typing import Type
+from typing import Type, Tuple, Dict
+from collections import namedtuple
 import requests
 from requests import Request
-from collections import namedtuple
+from src.errors import HttpRequestError
 
 class SwapiApiConsumer:
+    '''Class to consume swapi api with http requests'''
 
     def __init__(self):
         self.get_starships_response = namedtuple("GET_Starships", "status_code request response")
 
-    def get_starships(self, page):
+    def get_starships(self, page) -> Tuple[int, Type[Request], Dict]:
+        '''
+            request starships in pagination
+            :param - page: int with page of navegation
+            :return - Tuple with status_code, request, response attributes
+        '''
 
         req = requests.Request(
             method='GET',
@@ -18,10 +25,16 @@ class SwapiApiConsumer:
         req_prepared = req.prepare()
 
         response = self.__send_http_request(req_prepared)
+        status_code = response.status_code
 
-        return self.get_starships_response(
-            status_code=response.status_code, request=req, response=response.json()
-        )
+        if  200 <= status_code <= 299:
+            return self.get_starships_response(
+                status_code=status_code, request=req, response=response.json()
+            )
+        else:
+            raise HttpRequestError(
+                message=response.json()["detail"], status_code=status_code
+            )
         
 
     @classmethod
